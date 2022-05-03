86e620e60f0a2cb555805eda61fb4c2661c820a9
hendraaaa <hendraa3005@gmail.com>
2022-05-03 10:12:15 +0800

    midtrans callbak

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -7,17 +7,27 @@ use App\Http\Requests\User\Checkout\Store;
 use App\Mail\Checkout\AfterCheckout;
 use App\Models\Camp;
 use App\Models\Checkout;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
+use Midtrans;
+use SebastianBergmann\Exporter\Exporter;
+use Str;
 
 class CheckoutController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        Midtrans\Config::$serverKey = env('MIDTRANS_SERVERKEY');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
+        // Set sanitization on (default)
+        Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
+        // Set 3DS transaction for credit card to true
+        Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
+    }
+
     public function index()
     {
         //
@@ -62,6 +72,8 @@ class CheckoutController extends Controller
 
         //create checkout
         $checkout = Checkout::create($data);
+
+        $this->getSnapRedirect($checkout);
 
         //sending email
         Mail::to(Auth::user()->email)->send(new AfterCheckout($checkout));
@@ -116,6 +128,127 @@ class CheckoutController extends Controller
 
     public function success()
     {
+        return view('checkout.success');
+    }
+
+    public function getSnapRedirect(Checkout $checkout)
+    {
+        $checkout->update([
+            'midtrans_booking_code' => $checkout->id . '-' . Str::random(5)
+        ]);
+
+        // Fill transaction details
+        $transaction_details = array(
+            'order_id' => $checkout->midtrans_booking_code,
+            'gross_amount' => $checkout->Camp->price * 1000
+        );
+
+        // Mandatory for Mandiri bill payment and BCA KlikPay
+        // Optional for other payment methods
+        $item_details[] = [
+            "id" => $checkout->midtrans_booking_code,
+            "price" => $checkout->Camp->price * 1000,
+            "quantity" => 1,
+            "name" => "Payment for {$checkout->Camp->title} Camp"
+        ];
+
+        // Optional
+        $billing_address = array(
+            'first_name'    => $checkout->User->name,
+            'last_name'     => "",
+            'address'       => $checkout->User->address,
+            'city'          => "",
+            'postal_code'   => "",
+            'phone'         => $checkout->User->phone,
+            'country_code'  => 'IDN'
+        );
+
+        // Optional
+        $shipping_address = array(
+            'first_name'    => $checkout->User->name,
+            'last_name'     => "",
+            'address'       => $checkout->User->address,
+            'city'          => "",
+            'phone'         => $checkout->User->phone,
+            'postal_code'   => "",
+            'country_code'  => 'IDN'
+        );
+
+        $customer_details = array(
+            'first_name'    => $checkout->User->name, //optional
+            'last_name'     => "", //optional
+            'email'         => $checkout->User->email, //mandatory
+            'phone'         => $checkout->User->phone, //mandatory
+            'billing_address'  => $billing_address, //optional
+            'shipping_address' => $shipping_address //optional
+        );
+
+        // Fill transaction details
+        $params = array(
+            'transaction_details' => $transaction_details,
+            'customer_details' => $customer_details,
+            'item_details' => $item_details,
+        );
+
+        try {
+            // Get Snap Payment Page URL
+            $paymentUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
+            $checkout->midtrans_url = $paymentUrl;
+            $checkout->save();
+
+            // Send to Snap Payment Page
+            return $paymentUrl;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+    public function midtransCallback(Request $request)
+    {
+        $notif = new Midtrans\Notification();
+
+        $transaction_status = $notif->transaction_status;
+        $fraud = $notif->fraud_status;
+
+        $checkout_id = explode('-',$notif->order_id)[0];
+        $checkout = Checkout::find($checkout_id);
+
+        if ($transaction_status == 'capture') {
+            if ($fraud == 'challenge') {
+                // TODO Set payment status in merchant's database to 'challenge'
+                $checkout->payment_status='pending';
+            } else if ($fraud == 'accept') {
+                // TODO Set payment status in merchant's database to 'success'
+                $checkout->payment_status = 'paid';
+
+            }
+        } else if ($transaction_status == 'cancel') {
+            if ($fraud == 'challenge') {
+                // TODO Set payment status in merchant's database to 'failure'
+                $checkout->payment_status = 'failed';
+
+            } else if ($fraud == 'accept') {
+                // TODO Set payment status in merchant's database to 'failure'
+                $checkout->payment_status = 'failed';
+
+            }
+        } else if ($transaction_status == 'deny') {
+            // TODO Set payment status in merchant's database to 'failure'
+            $checkout->payment_status = 'failed';
+
+        } else if ($transaction_status == 'settlement') {
+            // TODO set payment status in merchant's database to 'Settlement'
+            $checkout->payment_status = 'paid';
+
+        } else if ($transaction_status == 'pending') {
+            // TODO set payment status in merchant's database to 'Pending'
+            $checkout->payment_status = 'pending';
+
+        } else if ($transaction_status == 'expire') {
+            // TODO set payment status in merchant's database to 'expire'
+            $checkout->payment_status = 'failed';
+        }
+
+        $checkout->save();
         return view('checkout.success');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,9 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-
+//socialite routes
+Route::get('payment/success',[UserController::class,'midtransCallback']);
+Route::post('payment/success',[UserController::class,'midtransCallback']);
 
 
 //sosialite routes
